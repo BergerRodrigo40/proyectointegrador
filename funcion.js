290e0982d014717e0aa32416667a3e970c165b34
BergerRodrigo40 <123420620+BergerRodrigo40@users.noreply.github.com>
2023-01-23 18:58:12 -0300

    Add files via upload

---
+++ funcion.js
@@ -0,0 +1,44 @@
+function mos_dat() {
+    document.getElementById('dat').style.display = 'block';
+    document.getElementById('ob').style.display = 'none';
+    document.getElementById('ex').style.display = 'none';
+    document.getElementById('fo').style.display = 'none';
+    document.getElementById('cua').style.display = 'none';
+
+}
+
+function mos_ob() {
+    document.getElementById('dat').style.display = 'none';
+    document.getElementById('ob').style.display = 'block';
+    document.getElementById('ex').style.display = 'none';
+    document.getElementById('fo').style.display = 'none';
+    document.getElementById('cua').style.display = 'none';
+
+}
+
+function mos_ex() {
+    document.getElementById('dat').style.display = 'none';
+    document.getElementById('ob').style.display = 'none';
+    document.getElementById('ex').style.display = 'block';
+    document.getElementById('fo').style.display = 'none';
+    document.getElementById('cua').style.display = 'none';
+
+}
+
+function mos_fo() {
+    document.getElementById('dat').style.display = 'none';
+    document.getElementById('ob').style.display = 'none';
+    document.getElementById('ex').style.display = 'none';
+    document.getElementById('fo').style.display = 'block';
+    document.getElementById('cua').style.display = 'none';
+
+}
+
+function mos_cua() {
+    document.getElementById('dat').style.display = 'none';
+    document.getElementById('ob').style.display = 'none';
+    document.getElementById('ex').style.display = 'none';
+    document.getElementById('fo').style.display = 'none';
+    document.getElementById('cua').style.display = 'block';
+
+}
